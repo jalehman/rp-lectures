9fd7eb469f2e05361e47b2d83cb29ab352775d12
Josh Lehman <jalehman37@gmail.com>
2016-01-11 22:47:33 -0800

    add in-class demo

--- functions/example/refactor-demo.js
+++ functions/example/refactor-demo.js
@@ -1,19 +1,59 @@
-// 1. Compute the sum of squares up to `n`, where n is 10.
-var n = 10;
-var i = 0;
-var result = 0;
-while (i < n) {
-  result = result + (i * i);
-  i++;
+function accumulate(a, b, combine, start) {
+  var result = start;
+  for(var i = a; i < b; i++) {
+    // combine should "combine" the previous result with the next i, returning
+    // the "next" result
+    result = combine(result, i);
+  }
+  return result;
 }
-console.log(result);
+
+function factorial(n) {
+  var combine = function(result, i) {
+    return result * i;
+  };
+  return accumulate(1, n, combine, 1);
+}
+
+function expt(base, exp) {
+  var combine = function(result, i) {
+    return result * base;
+  };
+  return accumulate(0, exp, combine, 1);
+}
+
+function summation(term, next, a, b) {
+  var combine = function(result, i) {
+    return result + term(i);
+  };
+  return accumulate(a, b, combine, 0);
+}
+
+function square(x) { return expt(x, 2); }
+
+function cube(x) { return expt(x, 3); }
+
+// 1. Compute the sum of squares up to `n`, where n is 10.
+function sumOfSquares(n) {
+  var next = function(i) { return i + 1; };
+  return summation(square, next, 5, n);
+}
+
+sumOfSquares(10);
 
 // 2. How about the sum of cubes of even numbers up to 10?
-n = 10;
-i = 0;
-result = 0;
-while (i < n) {
-  result = result + (i * i * i);
-  i = i + 2;
+function sumEvenCubes(n) {
+  var next = function(i) { return i + 2; };
+  return summation(cube, next, 0, n);
 }
-console.log(result);
+
+sumEvenCubes(10);
+
+// 3. Let's compute the sum of factorials from 3 to 11;
+
+function sumFactorials(a, b) {
+  var next = function(i) { return i + 1; };
+  return summation(factorial, next, a, b);
+}
+
+sumFactorials(3, 11);
